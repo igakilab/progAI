b078f61a3c575ca1f46439446bede8d695f59f3e
Hiroshi Igaki <github@musictale.net>
2017-07-05 16:31:14 +0900

    Cとexeファイルの情報を分けて収集するように変更

--- prog2mongo.py
+++ prog2mongo.py
@@ -22,11 +22,25 @@ class MongoController:
         print(post_id)
 
 #各ファイルの情報を追加
-def add_file_status(sid,file,filepath):
+def add_cfile_status(sid,file,filepath):
     update_time = datetime.datetime.fromtimestamp(os.stat(filepath).st_mtime)
     print(filepath)
     contents = open(filepath).read()
-    file_dict = {'sid':sid,'filename':file,'update_time':update_time,'contents':contents}
+    file_dict = {'sid':sid,'source':file,'source_update_time':update_time,'contents':contents}
+    print(file_dict)
+    mc = MongoController()
+    mc.add_post(file_dict)
+
+#各ファイルの情報を追加
+def add_exefile_status(sid,file,filepath):
+    create_time = datetime.datetime.fromtimestamp(os.stat(filepath).st_mtime)
+    for line in open(filepath):
+        line = line[:-1]
+        if line.split()[2] == 'src':
+            source = line.split()[3]
+            #ソースファイルが作成された日時
+            source_update_time = datetime.datetime.strptime(' '.join(line.split()[0:2]),'%Y/%m/%d %H:%M:%S')
+    file_dict = {'sid':sid,'exefile':file,'source':source,'exe_create_time':create_time,'source_update_time':source_update_time}
     print(file_dict)
     mc = MongoController()
     mc.add_post(file_dict)
@@ -36,10 +50,15 @@ def add_sid_dir(sid,siddir):
     for sfile in os.listdir(siddir):
         if re.compile(r'^_|^\.|^#|~$|.pdf$').search(sfile):
             pass
-        elif re.compile(r'.c$|.exe.txt$').search(sfile):
+        elif re.compile(r'.c$').search(sfile):
             sourcefile = siddir+'/'+sfile
             if os.path.isdir(sourcefile)!=True:
-                add_file_status(sid,sfile,siddir+'/'+sfile)
+                add_cfile_status(sid,sfile,siddir+'/'+sfile)
+
+        elif re.compile(r'.exe.txt$').search(sfile):
+            sourcefile = siddir+'/'+sfile
+            if os.path.isdir(sourcefile)!=True:
+                add_exefile_status(sid,sfile,siddir+'/'+sfile)
 
 #showlogall.txt内の課題毎コンパイル情報を追加
 def add_showlog(showlogfile):
@@ -48,7 +67,7 @@ def add_showlog(showlogfile):
         line = line[:-1]
         if line.startswith('=='):
             try:
-                #print(line.split()[2][0:16])
+                #"=="で始まる行からチェック時刻を取得する
                 check_date = datetime.datetime.strptime(line.split()[2][0:16],'%Y-%m%d-%H%M%S')
                 print(check_date)
             except (ValueError):
@@ -78,7 +97,7 @@ def main(args):
                 if os.path.isdir(siddir)==True:
                     add_sid_dir(sid,siddir)
                 elif sid == 'showlogall.txt':                    
-                    add_showlog(siddir)
+                    pass #add_showlog(siddir)
 
 if __name__ == '__main__':
     main(sys.argv)
